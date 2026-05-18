75a80cfbf63764a9caf69eade398b783d25f94d5
Z User <z@container>
2026-05-18 09:57:58 +0000

    fix: separate server/client components for calidad/auditoria (dynamic export only works in server components)

--- src/app/(app)/calidad/auditoria/page.tsx
+++ src/app/(app)/calidad/auditoria/page.tsx
@@ -1,16 +1,7 @@
-'use client'
+import { AuditoriaOperadorPageClient } from './auditoria-client'
 
 export const dynamic = 'force-dynamic'
 
-import { useOperador } from '@/components/providers/auth-provider'
-import { EditableScreenWrapper } from '@/components/ui/editable-screen'
-import { AuditoriaOperadorModule } from '@/modules-pending/auditoria-operador'
-
 export default function Page() {
-  const operador = useOperador()
-  return (
-    <EditableScreenWrapper moduloId="auditoriaOperador" operador={operador}>
-      <AuditoriaOperadorModule operador={operador} />
-    </EditableScreenWrapper>
-  )
+  return <AuditoriaOperadorPageClient />
 }
